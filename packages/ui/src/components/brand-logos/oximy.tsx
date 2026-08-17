d9d33babcc48d3d609bd4399f08d548a2baf71d5
namanambavi <nmambavi@gmail.com>
2026-08-17 23:32:45 +0000

    feat: add Oximy header branding

---
+++ packages/ui/src/components/brand-logos/oximy.tsx
@@ -0,0 +1,33 @@
+import type * as React from "react";
+
+const OximyLogo = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		width={91}
+		height={91}
+		viewBox="0 0 91 91"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		aria-hidden="true"
+		focusable="false"
+		{...props}
+	>
+		<rect width="90.3704" height="90.3704" fill="#FF4D00" />
+		<g fill="white">
+			<rect x="13.3906" y="26.7764" width="10.0412" height="10.0412" />
+			<rect x="13.3906" y="40.1643" width="10.0412" height="10.0412" />
+			<rect x="13.3906" y="53.553" width="10.0412" height="10.0412" />
+			<rect x="26.7773" y="13.3877" width="10.0412" height="10.0412" />
+			<rect x="26.7773" y="66.9407" width="10.0412" height="10.0412" />
+			<rect x="40.168" y="13.3877" width="10.0412" height="10.0412" />
+			<rect x="40.168" y="40.1643" width="10.0412" height="10.0412" />
+			<rect x="40.168" y="66.9407" width="10.0412" height="10.0412" />
+			<rect x="53.5508" y="13.3877" width="10.0412" height="10.0412" />
+			<rect x="53.5508" y="66.9407" width="10.0412" height="10.0412" />
+			<rect x="66.9414" y="26.7764" width="10.0412" height="10.0412" />
+			<rect x="66.9414" y="40.1643" width="10.0412" height="10.0412" />
+			<rect x="66.9414" y="53.553" width="10.0412" height="10.0412" />
+		</g>
+	</svg>
+);
+
+export default OximyLogo;
